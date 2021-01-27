f2a85b9cc3d81bd6e1e4c2d6a255d09311ecf638
geofell <geofell22@gmail.com>
2021-01-27 18:39:26 -0500

    added jquery to get the city name from the input box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
 var apiKey = "37168d7be793003143b3c0b30de5bedc";
 var cityName = "Atlanta";
-var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
 
 $("#search-button").on("click", function () {
+  console.log($("#search-city").val());
+  cityName = $("#search-city").val();
+  var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
   $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
     console.log(response);
-    console.log($("#search-city").val());
-    console.log(response.name);
+    console.log("City name = " + response.name);
   });
 });
