550d9bb14f74e92af0fa0d120a44f59046a3b1ec
geofell <geofell22@gmail.com>
2021-01-29 16:49:34 -0500

    add the city history and local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,57 @@
 var apiKey = "37168d7be793003143b3c0b30de5bedc";
 var cityName = "Atlanta";
+var cityHistorySaved = [];
+if (localStorage.getItem("cityHistoryStorage")) {
+  cityHistorySaved = JSON.parse(localStorage.getItem("cityHistoryStorage"));
+  for (var i = 0; i < cityHistorySaved.length; i++) {
+    addCityHistory(cityHistorySaved[i]);
+  }
+}
 
 $("#search-button").on("click", function () {
   console.log($("#search-city").val());
-  cityName = $("#search-city").val();
-  var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+  var searchCityInput = $("#search-city").val();
+
+  if (cityHistorySaved.indexOf(searchCityInput) == -1) {
+    cityHistorySaved.push(searchCityInput);
+    addCityHistory(searchCityInput);
+    localStorage.setItem(
+      "cityHistoryStorage",
+      JSON.stringify(cityHistorySaved)
+    );
+  }
+  console.log(cityHistorySaved);
+
+  getWeather(searchCityInput);
+});
+
+$(".city-history").on("click", "li", function () {
+  console.log($(this).text());
+  var searchCityClick = $(this).text();
+  getWeather(searchCityClick);
+});
+
+function addCityHistory(cityName) {
+  var liTag = $("<li class='city-history-name' value='" + cityName + "'>")
+    .addClass("list-group-item list-group-item-action")
+    .text(cityName);
+  $(".city-history").prepend(liTag);
+  // liTag.html(cityName);
+}
+function getWeather(citySearch) {
+  var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}`;
+
   $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
     console.log(response);
-    console.log("City name = " + response.name);
-    var pTag = $("<p>");
-    $("#city-list").prepend(pTag);
-    pTag.html(response.name);
-
-    var cityTemp = (parseInt(response.main.temp) - 273.15) * 9/5 + 32;
-    $("#weather-details").html(cityTemp);
+    var cityNameAPI = response.name;
+    var cityTemp =
+      Math.floor((parseInt(response.main.temp) - 273.15) * 9) / 5 + 32;
+    $(".city-name").html(`City: ${cityNameAPI}`);
+    $(".city-temp").html(`Temperature: ${cityTemp} F`);
+    $(".city-humidity").html(`Humidity: ${response.main.humidity} %`);
+    $(".city-wind-speed").html(`City: ${response.wind.speed} MPH`);
   });
-});
+}
